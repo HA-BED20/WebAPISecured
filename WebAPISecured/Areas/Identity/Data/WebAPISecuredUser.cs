13ffdcd7937061ffc03fec9287d17297f437afe6
Claes Engelin <claes.engelin@zocom.se>
2021-06-08 11:35:30 +0200

    Add project files.

---
+++ WebAPISecured/Areas/Identity/Data/WebAPISecuredUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAPISecured.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the WebAPISecuredUser class
+    public class WebAPISecuredUser : IdentityUser
+    {
+    }
+}
---
+++ WebAPISecured/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPISecured.Areas.Identity.Data;
+using WebAPISecured.Data;
+
+[assembly: HostingStartup(typeof(WebAPISecured.Areas.Identity.IdentityHostingStartup))]
+namespace WebAPISecured.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<WebAPISecuredContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("WebAPISecuredContextConnection")));
+
+                services.AddDefaultIdentity<WebAPISecuredUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<WebAPISecuredContext>();
+            });
+        }
+    }
+}
